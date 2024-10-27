4c0dc032766c347bbbd1534080197519fb92ebbc
davidonio123 <davidebarretta123@gmail.com>
2024-10-27 11:54:01 +0100

    Update db_connection.php

--- db/db_connection.php
+++ db/db_connection.php
@@ -1,12 +1,13 @@
 <?php
 
 //PDO Connection
-//include("./db_date.php");
 
-$servername="learnenglisheasily.net";
-$username="u235686219_davidechristia";
-$passworddb="Ciaomamma_280624";
-$dbname="u235686219_utenti";
+// include("./db_date.php");
+
+$servername="localhost";
+$username="root";
+$passworddb="";
+$dbname="utenti";
 
 try{
     $db = new PDO("mysql:=$servername;dbname=$dbname", $username, $passworddb);
