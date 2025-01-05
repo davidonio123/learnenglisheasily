dc38b9ac7dd03d3ee0e7a26aa5e12f139ffa2ed8
davidonio123 <davidebarretta123@gmail.com>
2025-01-05 15:59:00 +0100

    commit

--- index.php
+++ index.php
@@ -1,1 +1,5 @@
-<?php ?>
+<?php define("TITLE", "prova1"); ?>
+
+<?php include('./layouts/header.php') ?>
+<?php include('./sezioni nuove/sezione1.php') ?>
+<?php include('./layouts/foother.php') ?>
