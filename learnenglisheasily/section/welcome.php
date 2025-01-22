1718bfc2ef2f754d61a366527fff5ed88ec01b59
davidonio123 <davidebarretta123@gmail.com>
2025-01-22 20:39:43 +0100

    welcome

--- learnenglisheasily/section/welcome.php
+++ learnenglisheasily/section/welcome.php
@@ -2,28 +2,36 @@
     <p class="text mainColor ">Welcome to your private page</p>
 </div>
 
-<div class="container">
-    <!-- profilo utente -->
-    <div class="row" style="border-radius: 12px;height:auto; overflow: hidden; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
-        <!-- immagine profilo -->
-        <div class="col-md-3 m-0 p-0" style="height: 1fr; width: 1fr;">
-            <img style="height: 100%; width: 100%;" class="img fluid" src="https://gravatar.com/avatar/697cc154031eb718882903ac1ff4dd2a?d=identicon&s=256" alt="image not found">
+<div class="w-100">
+    <div class="container">
+        <!-- profilo utente -->
+        <div class="row" style="border-radius: 12px;height:auto; overflow: hidden; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+            <!-- immagine profilo -->
+            <div class="col-md-3 m-0 p-0 d-flex justify-content-center" style="height: 1fr; width: 1fr;">
+                <img style="height: 100%; width: 100%;" class="img fluid" src="https://gravatar.com/avatar/697cc154031eb718882903ac1ff4dd2a?d=identicon&s=256" alt="image not found">
+            </div>
+            <!-- informazioni -->
+            <div class="col-md-8 p-4">
+                <p class="text mainColor mid">
+                    Davide Barretta 4BI
+                    <span class="badge badge-novizio ml-2">Studente</span>
+                    <span class="badge badge-novizio ml-2">Programmatore</span>
+                </p>
+                <p class="text lightGray down mt-3">
+                    Email: davide.barretta21@iti-marconi.edu.it
+                </p>
+                <p class="text lightGray mt-3">
+                    240 punti
+                </p>
+                <div class="link">
+                    <a href="#">Modifica profilo</a>
+                </div>
+            </div>
         </div>
-        <!-- informazioni -->
-        <div class="col-md-8 p-4">
-            <p class="text mainColor mid">
-                Davide Barretta 4BI
-                <span class="badge badge-novizio ml-2">Studente</span>
-                <span class="badge badge-novizio ml-2">Programmatore</span>
-            </p>
-            <p class="text lightGray down mt-3">
-                Email: davide.barretta21@iti-marconi.edu.it
-            </p>
-            <p class="text lightGray mt-3">
-                240 punti
-            </p>
-            <div class="link">
-                <a href="#">Modifica profilo</a>
+        <!-- esercizi svolti -->
+        <div class="row mt-3 p-4" style="border-radius: 12px;height:auto; overflow: hidden; box-shadow: 0 0 10px rgba(0,0,0,0.1);">
+            <p class="text mainColor mid mb-3">Esercizi svolti</p>
+                <?php include("./function/welcome.php") ?>
             </div>
         </div>
     </div>
@@ -36,8 +44,20 @@
         transform: translateY(-3px);
     }
 
-    .link{
+    .link {
         position: relative;
         margin-top: 130px;
+    }
+
+    .badge-success {
+        background-color: #28a745;
+    }
+
+    .badge-warning {
+        background-color: #ffc107;
+    }
+
+    .badge-danger {
+        background-color: #dc3545;
     }
 </style>
