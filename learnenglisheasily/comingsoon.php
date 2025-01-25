55e7ab56ab4b7b22a147a50cbd128eadaaea1c4c
MakaPrismax <164096322+MakaPrismax@users.noreply.github.com>
2025-01-25 18:33:50 +0100

    Coming soon

--- learnenglisheasily/comingsoon.php
+++ learnenglisheasily/comingsoon.php
@@ -1,2 +1,4 @@
-in qeusta pagina deve esserci un bottone indietro che ritorna alla home e un testo coming soon <br>
-attenzione a fare tutto resaizable anche per telefono
+<?php define("TITLE", "coming soon"); ?>
+
+
+<?php include('./section/comingsoon.php') ?>
--- learnenglisheasily/layouts/header.php
+++ learnenglisheasily/layouts/header.php
@@ -13,9 +13,8 @@
     <!--BOOTSTRAP-->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <!-- link animazioni -->
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/animation.style@1.0.0/animation.min.css">
+        <!--ANIMATION-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css">
 
         <title><?PHP echo TITLE ?></title>
 </head>
@@ -24,19 +23,19 @@
         <div class="menu-container">
             <div class="container">
                 <button class="menu-button">
-                    <a href="./" class="text lightGray down">Home</a>
+                    <a href="./index.php" class="text lightGray down">Home</a>
                 </button>
                 <button class="menu-button">
-                    <a href="./feedback" class="text lightGray down">Feedback</a>
+                    <a href="./feedback.php" class="text lightGray down">Feedback</a>
                 </button>
                 <button class="menu-button">
-                    <a href="./grammar" class="text lightGray down">Grammar</a>
+                    <a href="#" class="text lightGray down">Grammar</a>
                 </button>
                 <button class="menu-button">
-                    <a href="./vocabulary" class="text lightGray down">Vocabulary</a>
+                    <a href="#" class="text lightGray down">Vocabulary</a>
                 </button>
                 <button class="menu-button">
-                    <a href="./aboutus" class="text lightGray down">About Us</a>
+                    <a href="./aboutus.php" class="text lightGray down">About Us</a>
                 </button>
             </div>
         </div>
