5948d0c076361900057255a27847736d14fa24c9
MakaPrismax <164096322+MakaPrismax@users.noreply.github.com>
2025-02-25 21:54:57 +0100

    Some Changes

---
+++ learnenglisheasily2/client/vocabulary.php
@@ -0,0 +1,5 @@
+<?php define("TITLE", "vocabulary"); ?>
+
+<?php include('./client/layouts/header.php') ?>
+<?php include('./client/section/vocabulary.php') ?>
+<?php include('./client/layouts/foother.php') ?>
